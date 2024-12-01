90de7f46568b8b7144095ade8d834ee9386b3ce3
aceJacek <acejacek@gmail.com>
2024-12-01 20:15:27 +0100

    Python version for day 1

---
+++ 2024/day01.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+def load_lists(filename):
+    file = open(filename, "r");
+    list_a = []
+    list_b = []
+    for line in file.readlines():
+        tmp = line.split("  ")
+        try:
+            list_a.append(eval(tmp[0]))
+            list_b.append(eval(tmp[1]))
+        except:
+            pass
+
+    list_a.sort()
+    list_b.sort()
+    return list_a, list_b
+
+
+def calculate_sum(list_a, list_b):
+    list_sum = 0
+    for i in range(len(list_a)):
+        list_sum += abs(list_a[i] - list_b[i])
+
+    return list_sum
+
+
+def calculate_similarity(list_a, list_b):
+    list_similarity_score = 0
+    for elem in list_a:
+        list_similarity_score += elem * list_b.count(elem)
+
+    return list_similarity_score
+
+
+l1, l2 = load_lists("day01.test")
+assert calculate_sum(l1, l2) == 11, "Wromg sum"
+assert calculate_similarity(l1, l2) == 31, "Wrong similarity"
+
+l1, l2 = load_lists("day01.txt")
+print("Part 1: sum of differences is", calculate_sum(l1, l2))
+print("Part 2: similarity score is", calculate_similarity(l1, l2))
+
