1a62c0414dab94956f9c7dadb54af7c9d6deb4b1
aceJacek <acejacek@gmail.com>
2024-12-07 23:01:24 +0100

    day 7, part 2

--- 2024/day07.py
+++ 2024/day07.py
@@ -1,17 +1,32 @@
-
-def multiply_or_plus(value, numbers, prev):
-    if len(numbers) == 0:
+def multiply_or_add(target, numbers, prev):
+    """recursively add/multiple consequect elements, testing if reaches target"""
+    # did we already overshoot the target?
+    if prev > target:
         return False
-    # if tested all numbers and reach the value -> success
-    if len(numbers) == 1 and \
-       (prev + numbers[0] == value or \
-        prev * numbers[0] == value):
-        return True
 
-    return multiply_or_plus(value, numbers[1:], prev + numbers[0]) or \
-           multiply_or_plus(value, numbers[1:], prev * numbers[0])
+    # end reached; do we have a match?
+    if len(numbers) == 0:
+        return prev == target
 
-def calculate(filename):
+    return multiply_or_add(target, numbers[1:], prev + numbers[0]) or \
+           multiply_or_add(target, numbers[1:], prev * numbers[0])
+
+def multiply_or_add_or_merge(target, numbers, prev):
+    """recursively add/multiple/merge consequect elements, testing if reaches target"""
+    def merge(a, b):
+        return int(str(a) + str(b))
+
+    if prev > target:
+        return False
+
+    if len(numbers) == 0:
+        return prev == target
+
+    return multiply_or_add_or_merge(target, numbers[1:], prev + numbers[0]) or \
+           multiply_or_add_or_merge(target, numbers[1:], prev * numbers[0]) or \
+           multiply_or_add_or_merge(target, numbers[1:], merge(prev, numbers[0]))
+
+def calculate(filename, part = 1):
     with open(filename, "r") as file:
         lines = file.readlines()
 
@@ -21,7 +36,10 @@ def calculate(filename):
         numbersStr = line.split(":")[1]
         numbers = list(map(int, numbersStr.split()))
 
-        if multiply_or_plus(testValue, numbers[1:], numbers[0]) == True:
+        if part == 1 and multiply_or_add(testValue, numbers[1:], numbers[0]):
+            totalResult += testValue
+
+        if part == 2 and multiply_or_add_or_merge(testValue, numbers[1:], numbers[0]):
             totalResult += testValue
 
     return totalResult
@@ -29,3 +47,5 @@ def calculate(filename):
 assert calculate("day07.test") == 3749
 print("Part 1:", calculate("day07.txt"))
 
+assert calculate("day07.test", part = 2) == 11387
+print("Part 2:", calculate("day07.txt", part = 2))
