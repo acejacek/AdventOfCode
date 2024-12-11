c52b4b48a72a0d7761afa67528dd47bfedcf4c5d
aceJacek <acejacek@gmail.com>
2024-12-11 22:52:49 +0100

    Day3, part 2 + code comments
    Converstion from String to Int is not soo sexy in Julia

--- 2018/day03.jl
+++ 2018/day03.jl
@@ -4,7 +4,8 @@ function calculate1(filename)
     input = readlines(filename)
 
     fabric = Dict();
-    
+    claims = []
+
     for line in input
         id, a ,b ,c ,d = split(line, ('#',' ','@', ':', ',', 'x'), keepempty=false) 
         # I have no idea how to smartly convert procucts of split() to Int
@@ -12,6 +13,8 @@ function calculate1(filename)
         B = parse(Int, b)
         C = parse(Int, c) - 1
         D = parse(Int, d) - 1
+        push!(claims, (id, A, B, C, D))
+        # put all claimed locations to the dictionary, with counter
         for x = A:A + C
             for y = B:B + D
                 claimCount = get(fabric, (x, y), 0)
@@ -20,12 +23,34 @@ function calculate1(filename)
             end
         end
     end
-    part1 = length([loc for (loc, claimCount) in fabric if claimCount > 1])
+    # count all locations claimed more that once
+    part1 = length([loc for (loc, claimCount) in fabric if claimCount > 1]) 
 
-    part2 = 0
-    return (part1, part2)
+    part2::String = ""
+    # scan all claims
+    for (id, A, B, C, D) in claims
+        overlap = false
+        for x = A:A + C
+            for y = B:B + D
+                claimCount = get(fabric, (x, y), 0)
+                if claimCount > 1
+                    overlap = true
+                    break
+                end
+            end
+        end
+        # answer is claims without overlap
+        if !overlap
+            part2 = id
+            break
+        end
+    end
+        
+    (part1, part2)
 end
 
-@assert calculate1("day03.test") == (4, 0)
+@assert calculate1("day03.test") == (4, "3")
 (a, b) = calculate1("day03.txt")
-println("Part 1 :", a)
+println("Part 1: ", a)
+println("Part 2: ", b)
+
