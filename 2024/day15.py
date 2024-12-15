6833a3ae02d5170b1c92d746140bb39ef14107f7
aceJacek <acejacek@gmail.com>
2024-12-15 14:35:15 +0100

    Day 15, part 2
    Change from list to set seems to slow down the algo, but I don't want to
    chage it back again

--- 2024/day15.py
+++ 2024/day15.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 
-class Wall:
-    def __init__(self, pos):
-        self.pos = pos
-
 class Box:
     def __init__(self, pos):
         self.pos = pos
@@ -14,7 +10,7 @@ class Box:
     def colide(self, pos):
         return self.pos == pos
 
-    def move(self, direction):
+    def move(self, direction, check = False):
         if direction == ">":
             newPos = (self.pos[0] + 1, self.pos[1])
         elif direction == "<":
@@ -25,21 +21,23 @@ class Box:
             newPos = (self.pos[0], self.pos[1] - 1)
 
         for wall in walls:
-            if wall.pos == newPos:
+            if wall == newPos:
                 return False
 
         for box in boxes:
+            if box.pos == self.pos: continue
             if box.colide(newPos):
-                if not box.move(direction):
+                if not box.move(direction, check):
                     return False
-        self.pos = newPos
+
+        if not check:
+            self.pos = newPos
         return True
 
 class Robot(Box):
     pass
 
 class WideBox(Box):
-
     def __init__(self, pos):
         self.pos = pos
         self.half = (pos[0] + 1, pos[1])
@@ -47,7 +45,7 @@ class WideBox(Box):
     def colide(self, pos):
         return self.pos == pos or self.half == pos
 
-    def move(self, direction):
+    def move(self, direction, check = False):
         if direction == ">":
             newPos = (self.pos[0] + 1, self.pos[1])
         elif direction == "<":
@@ -60,30 +58,31 @@ class WideBox(Box):
         newHalf = (newPos[0] + 1, newPos[1])
 
         for wall in walls:
-            if wall.pos == newPos or wall.pos == newHalf:
+            if wall == newPos or wall == newHalf:
                 return False
 
         for box in boxes:
             if box.pos == self.pos: continue
             if box.colide(newPos) or box.colide(newHalf):
-                if not box.move(direction):
+                if not box.move(direction, check):
                     return False
-        self.pos = newPos
-        self.half = newHalf
+
+        if not check:
+            self.pos = newPos
+            self.half = newHalf
         return True
 
 def calculate(filename, part = 1):
 
     global walls
-    walls = []
     global boxes
-    boxes = []
+    walls = set()
+    boxes = set()
     moveLoad = False
-    y = 0
     movements = ""
-    robot = 0
 
     with open(filename, "r") as file:
+        y = 0
         while line := file.readline():
             if line == "\n":
                 moveLoad = True
@@ -95,26 +94,44 @@ def calculate(filename, part = 1):
                 if part == 1:
                     for x, elem in enumerate(line):
                         if elem == "#":
-                            walls += [Wall((x, y))]
+                            walls.add((x, y))
                         if elem == "O":
-                            boxes += [Box((x, y))]
+                            boxes.add(Box((x, y)))
                         if elem == "@":
                             robot = Robot((x, y))
                 else:
                     for x, elem in enumerate(line):
                         x *= 2
                         if elem == "#":
-                            walls += [Wall((x, y))]
-                            walls += [Wall((x + 1, y))]
+                            walls.add((x, y))
+                            walls.add((x + 1, y))
                         if elem == "O":
-                            boxes += [WideBox((x, y))]
+                            boxes.add(WideBox((x, y)))
                         if elem == "@":
                             robot = Robot((x, y))
-                y += 1
+                y += 1  
 
-    for move in movements:
-        if move != "\n":
-            robot.move(move)
+    for direction in movements:
+        if direction != "\n":
+
+            if part == 1:
+                robot.move(direction)
+
+            # for part 2 i need to implement two pases for movement
+            else:
+                # firstly only checks, if movement is possible. In some cases 
+                # for movements up/down only part stacked of boxes can be moved
+                # wchich gives false reading, that total movement is possible
+                if robot.move(direction, check = True):
+                    robot.move(direction)
+            """otherwise thias happens:
+              stage 1   stage 2
+               ###      ###[]   <- this moved, because got push (flag "check" prevents that)
+                [][]     []     <- this stays, because is blocked
+                 []       []    <- this stays still too
+                 @        @
+                 ^
+            """
 
     return sum([box.gpsCoord() for box in boxes])
 
@@ -123,7 +140,8 @@ assert calculate("day15.test2") == 10092
 a = calculate("day15.txt")
 print("Part 1:", a)
 
-assert calculate("day15.test3", part = 2) == 618
+assert calculate("day15.test", part = 2) == 1751
 assert calculate("day15.test2", part = 2) == 9021
+assert calculate("day15.test3", part = 2) == 618
 a = calculate("day15.txt", part = 2)
 print("Part 2:", a)
