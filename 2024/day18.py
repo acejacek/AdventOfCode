f83884f9826dfb4bfb87a2fee6181aa52d02041d
acejacek <acejacek@gmail.com>
2024-12-21 10:27:39 +0100

    Day 18, final & fast
    Use binary search for part 2

--- 2024/day18.py
+++ 2024/day18.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
 import heapdict as hd
-from collections import deque
 
-def calc1(filename, finish, amount):
+def calc(filename, finish, amount):
 
     notvisited = hd.heapdict()
     visited = dict()
@@ -33,62 +32,29 @@ def calc1(filename, finish, amount):
             if  notvisited[n] > weight + 1:
                 notvisited[n] = weight + 1
 
-    return visited[finish]
-
-def calc2(filename, finish):
-
-    bytestable = []
-
-    with open(filename, "r") as file:
-        fall = 0
-        while line := file.readline():
-            x, y = map(int, line.split(","))
-            bytestable += [(x, y)]
-
-    f = 1024
-    blocked = False
-    while not blocked:
-
-        notvisited = hd.heapdict()
-        visited = dict()
-
-        for x in range(finish[0] + 1):
-            for y in range(finish[0] + 1):
-                if (x, y) not in bytestable[0:f]:
-                    notvisited[(x, y)] = float("inf")
-
-        notvisited[(0, 0)] = 0
-        while len(notvisited) > 0:
-
-            # find smallest distance node:
-            (x, y), weight = notvisited.popitem()
-            visited[(x, y)] = weight
-
-            arround = [(x + dx, y + dy) for dx, dy in [[-1, 0], [1, 0], [0, -1], [0, 1]]]
-            neighbours = [node for node in arround if node in notvisited]
-            for n in neighbours:
-                if  notvisited[n] > weight + 1:
-                    notvisited[n] = weight + 1
-        
-        f += 1
-        if visited[finish] == float("inf"):
-            blocked = True
-
-    print(bytestable[f - 2])
+    return (visited[finish], line)
 
 
 finish = (6,6)
-assert calc1("day18.test", finish, 12) == 22
+a, _ = calc("day18.test", finish, 12)
+assert a == 22
 
 finish = (70, 70)
-a = calc1("day18.txt", finish, 1024)
+a, _ = calc("day18.txt", finish, 1024)
 print("Part 1:", a)
 
-a = calc2("day18.txt", finish)
-exit()
+minimum = 1024
+maximum = 3451
 
-for f in range(1024, 3451):
-    if calc1("day18.txt", finish, f) == float("inf"):
-        print("Part 2:", f)
-        break
+while True:
+    f = (maximum - minimum) // 2 + minimum
+    (a, b) = calc("day18.txt", finish, f)
+    if a == float("inf"):
+        if minimum == f - 1:
+            print("Part 2:", f, b)
+            break
+        maximum = f
+    else:
+        minimum = f
+    
 
