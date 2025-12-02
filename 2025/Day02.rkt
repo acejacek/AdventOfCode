24ea6f03a41ed674ab10ca6f0a8f599abfdb991f
acejacek <acejacek@gmail.com>
2025-12-02 22:32:30 +0100

    part 1 fully working

--- 2025/Day02.rkt
+++ 2025/Day02.rkt
@@ -4,45 +4,38 @@
 "1698522-1698528" "446443-446449" "38593856-38593862" "565653-565659" 
 "824824821-824824827" "2121212118-2121212124"))
 
-(define from (string->number (car (string-split "11-22" "-"))))
-(define to (string->number(car(cdr (string-split "11-22" "-")))))
-
-(struct range (from to))
-
-(define r (range from to))
-
-;(range-from r)
-;(range-to r)
-
-(define (generate start end)
-  
-  (define (iter start end result)
-    (define (len number) (string-length (number->string number)))
-    (define (divisor len)
-      (expt 10 (quotient len 2)))
-    (define left (quotient start (divisor (len start))))
-    (define right (modulo start (divisor (len start))))
-    
-    (cond
-      [(> start end) result]
-      [else
-       (if (= left right)
-           (iter (add1 start) end (+ result start))
-           (iter (add1 start) end result))]))
-  (iter start end 0))
-
-(generate (range-from r) (range-to r))
+(define input (string-split (car (file->lines "Day02_input.txt")) ","))
 
 (define (process list)
+  
+  (define (generate start end)
+    (define (iter start end result)
+      (define len
+        (λ (n) (string-length (number->string n))))
+      (define divisor
+        (λ (l) (expt 10 (quotient l 2))))
+      (define left (quotient start (divisor (len start))))
+      (define right (modulo start (divisor (len start))))
+    
+      (cond
+        [(> start end) result]
+        [else
+         (if (= left right)
+             (iter (add1 start) end (+ result start))
+             (iter (add1 start) end result))]))
+    (iter start end 0))
+  
   (define (iterate list sum)
     (if (empty? list)
         sum
         (let()
           (define from (string->number (car (string-split (car list) "-"))))
-          (define to (string->number(car(cdr (string-split (car list) "-")))))
-          (struct range (from to))
-          (define r (range from to))
-          (iterate (cdr list) (+ sum (generate (range-from r) (range-to r)))))))
+          (define to (string->number(car (cdr (string-split (car list) "-")))))
+          (iterate (cdr list) (+ sum (generate from to))))))
   (iterate list 0))
 
-(process test-input)
+(display "Part 1: ")
+(if (= 1227775554 (process test-input))
+    (process input)
+    (error "Wrong"))
+
