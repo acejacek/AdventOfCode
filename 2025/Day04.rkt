6c0e8a135019cf39429c77895b2334142bf565fc
acejacek <acejacek@gmail.com>
2025-12-08 20:55:56 +0100

    Part 2 ready
    Super slow, but I have no idea how to keep everything unmuttable and
    keep performance high. All this arrays shuffling...

--- 2025/Day04.rkt
+++ 2025/Day04.rkt
@@ -17,46 +17,79 @@
 
 (define input (read-lines "Day04_input.txt"))
 
-; convert list of strings to 2D array, wuth 0 and 1s
-; pad it twith 0s rows and cols
+; convert list of strings to 2D array, with 0 and 1s
 (define diagram
   (λ (ls)
-    (define (prepare ls)
-      (list*->array
-       (for/list ([line ls])
-         (for/list ([element (string->list line)])
-           (if (char=? element #\@) 1 0))) number?)) ; convert to 0 and 1
-    (array-append* (list
-                    (array 0) ; pad cols
-                    (array-append* (list
-                                    (array 0) ; pad rows
-                                    (prepare ls)
-                                    (array 0)))
-                    (array 0)) 1))) 
-
-; selects array 3x3 around x y of arr
+    (list*->array
+     (for/list ([line ls])
+       (for/list ([element (string->list line)])
+         (if (char=? element #\@) 1 0))) number?))) ; convert to 0 and 1
+    
+; selects array 3x3 (in most cases) around x y of arr
 (define (pick-3 arr x y)
-  (let* ([row-from (sub1 y)]
-         [row-to (+ 2 y)]
-         [col-from (sub1 x)]
-         [col-to (+ 2 x)]
+  (let* ([size (vector-ref (array-shape arr) 0)] ; array is square, no need to check other axle
+         [clamp  ; make sure 3x3 is not going outside array area.
+          (λ (a) (cond
+            [(> a size) size]
+            [(negative? a) 0]
+            [else a]))]
+         [row-from (clamp (sub1 y))] 
+         [row-to (clamp (+ 2 y))]
+         [col-from (clamp (sub1 x))]
+         [col-to (clamp (+ 2 x))]
          [rows (in-range row-from row-to)]
          [cols (in-range col-from col-to)]
          [range (list rows cols)])
-    (array-slice-ref arr range)))  ; (row col)
+    (array-slice-ref arr range)))  ; (rows cols)
 
 (define (count inp)
   (let* ([arr (diagram inp)]
-         [size (sub1 (vector-ref (array-shape arr) 0))])
-    (for/sum ([col (in-range 1 size)])
-      (for/sum ([row (in-range 1 size)])
-        (if (zero? (array-count positive? (array-slice-ref arr (list col row))))
-            0
-            (if (> 5 (array-all-sum (pick-3 arr row col)))
-                1
-                0))))))
-             
+         [size (vector-ref (array-shape arr) 0)]
+         [processed-lists (for/list ([col (in-range 0 size)])
+                            (for/list ([row (in-range 0 size)])
+                              (if (zero? (array-count positive? (array-slice-ref arr (list col row))))
+                                  0
+                                  (if (> 5 (array-count positive? (pick-3 arr row col)))
+                                      1
+                                      0))))])
+
+    (for/sum ([ln processed-lists])
+      (for/sum ([element ln]) element))))
+    
+(define (count-2 inp)
+  (let* ([arr (diagram inp)]
+         [size (vector-ref (array-shape arr) 0)])
+    (define (remove-rolls arr removed)
+      (define processed-lists
+        (for/list ([col (in-range 0 size)])
+          (for/list ([row (in-range 0 size)])
+            (if (zero? (array-count positive? (array-slice-ref arr (list col row))))
+                0
+                (if (> 5 (array-count positive? (pick-3 arr row col)))
+                    1
+                    2)))))
+      (define result 
+        ; return pair (count of removed rolls . array with new layout)
+        (cons (for/sum ([ln processed-lists])
+                (foldl + 0 (filter odd? ln)))
+              (list*->array (for/list ([ln processed-lists])
+                              (for/list ([element ln])
+                                (cond
+                                  [(= element 2) 1]
+                                  [(= element 1) 0]
+                                  [else element]))) number?)))
+      (if (zero? (car result))
+          removed
+          (remove-rolls (cdr result) (+ removed (car result)))))
+    (remove-rolls arr 0)))
+
 (display "Part 1: ")         
 (if (= 13 (count test-input))
     (count input)
     (error "wrong result"))
+
+(display "Part 2: ")         
+(if (= 43 (count-2 test-input))
+    (count-2 input)
+    (error "wrong result"))
+
