6a4e04c0abd536e30509556cb86c42f4fd63e361
acejacek <acejacek@gmail.com>
2025-12-09 08:15:02 +0100

    Optimizations
    Simplified `pick-3` function, it's just returning single value instead
    of full array. Clear `cond` instead of chained `if`s.

--- 2025/Day04.rkt
+++ 2025/Day04.rkt
@@ -3,6 +3,7 @@
 (require math/array)
 (require 2htdp/batch-io)
 
+; map: rolls of paper in the warehouse
 (define test-input (list
                     "..@@.@@@@."
                     "@@@.@.@.@@"
@@ -25,7 +26,7 @@
        (for/list ([element (string->list line)])
          (if (char=? element #\@) 1 0))) number?))) ; convert to 0 and 1
     
-; selects array 3x3 (in most cases) around x y of arr
+; selects array 3x3 (in most cases) around x y of arr, and counts rolls in it
 (define (pick-3 arr x y)
   (let* ([size (sub1 (vector-ref (array-shape arr) 0))] ; array is square, no need to check other axle
          [clamp  ; make sure 3x3 is not going outside array area.
@@ -40,18 +41,17 @@
          [rows (in-inclusive-range row-from row-to)]
          [cols (in-inclusive-range col-from col-to)]
          [range (list rows cols)])
-    (array-slice-ref arr range)))  ; (rows cols)
+    (array-count positive? (array-slice-ref arr range))))  ; (rows cols)
 
 (define (count-1 inp)
   (let* ([arr (diagram inp)]
          [size (vector-ref (array-shape arr) 0)]
          [processed-lists (for*/list ([col (in-range 0 size)]
                                       [row (in-range 0 size)])
-                            (if (zero? (array-ref arr (vector col row)))
-                                0
-                                (if (> 5 (array-count positive? (pick-3 arr row col)))
-                                    1
-                                    0)))])
+                            (cond
+                              [(zero? (array-ref arr (vector col row))) 0] ; wos nothing, keep nothing
+                              [(> 5 (pick-3 arr row col)) 1] ; was less than 5 in 3x3, mark it
+                              [else 0]))]) ; ignore everything else
     (for/sum ([element processed-lists]) element)))
     
 (define (count-2 inp)
@@ -62,16 +62,15 @@
       (define processed-lists
         (for*/list ([col (in-range 0 size)] ; scan all coords in array
                     [row (in-range 0 size)])
-          (if (zero? (array-ref arr (vector col row)))  ; ignore empty areas
-              0
-              (if (> 5 (array-count positive? (pick-3 arr row col)))  ; chech if below 5 in surrounds
-                  1     ; 1 - good location
-                  2)))) ; 2 - bad location
+          (cond
+            [(zero? (array-ref arr (vector col row))) 0] ; ignore empty areas
+            [(> 5 (pick-3 arr row col)) 1] ; below 5 in 3x3, put 1 there
+            [else 2]))) ; 2 - bad location
       (define result 
         ; return pair (count of removed rolls . array with new layout)
-        (cons (foldl + 0 (filter odd? processed-lists))  ; sum up all good locations
+        (cons (foldl + 0 (filter odd? processed-lists))  ; sum up all 1s (good locations)
               (list->array (vector size size) ; create new array of #(size size)
-                           (map (λ (element)
+                           (map (λ (element) ; with this content
                                   (cond
                                     [(zero? element) 0]
                                     [else (sub1 element)])) ; filter removed rolls (chng 2 to 1 and 1 to 0)
@@ -85,7 +84,6 @@
 (if (= 13 (count-1 test-input))
     (displayln (count-1 input))
     (error "wrong result"))
-
 
 (display "Part 2: ")
 (if (= 43 (count-2 test-input))
