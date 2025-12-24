d7110878c18262ccd3a68499449b955a6ff7e7ec
acejacek <acejacek@gmail.com>
2025-12-24 23:08:18 +0100

    fixes

--- 2024/day20.py
+++ 2024/day20.py
@@ -2,7 +2,7 @@
 
 import heapdict as hd
 
-def calc(filename):
+def calc1(filename):
 
     nodes = set()
     walls = set()
@@ -41,7 +41,6 @@ def calc(filename):
             if  notvisited[n] > weight + 1:
                 notvisited[n] = weight + 1
 
-    baseTime = visited[end]
     scores = dict()
 
     for x, y in walls:
@@ -61,5 +60,80 @@ def calc(filename):
 
     return sum([qty for score, qty in scores.items() if score >= 100])
 
-a = calc("day20.txt")
-print("part 1:", a)
+def calc2(filename):
+
+    nodes = set()
+    walls = set()
+    
+    y = 0
+    with open(filename, "r") as file:
+        while line := file.readline():
+        
+            for x, elem in enumerate(line):
+                if elem == "#":
+                    walls.add((x, y))
+                if elem == "S":
+                    start = (x, y)
+                    nodes.add(start)
+                if elem == "E":
+                    end = (x, y)
+                    nodes.add(end)
+                if elem == ".":
+                    nodes.add((x, y))
+            y += 1
+
+    notvisited = hd.heapdict()
+    for node in nodes:
+        notvisited[(node)] = float("inf")
+    notvisited[start] = 0
+    visited = dict()
+    while len(notvisited) > 0:
+
+        # find smallest distance node:
+        (x, y), weight = notvisited.popitem()
+        visited[(x, y)] = weight
+
+        arround = [(x + dx, y + dy) for dx, dy in [[-1, 0], [1, 0], [0, -1], [0, 1]]]
+        neighbours = [node for node in arround if node in notvisited]
+        for n in neighbours:
+            if  notvisited[n] > weight + 1:
+                notvisited[n] = weight + 1
+
+    scores = dict()
+
+    def walk(x, y, minimum, step):
+
+        # too far
+        if step == 21: return 0
+        # ended up at the path
+        if (x, y) in visited:
+            # save means shortcut between locations + cost of walking through wall
+            save = visited[(x, y)] - minimum - 2
+            if save > 0:
+                soFarSaved = scores.get(save, 0)
+                scores[save] = soFarSaved + 1
+            return
+
+        arround = [(x + dx, y + dy) for dx, dy in [[-1, 0], [1, 0], [0, -1], [0, 1]]]
+        for nx, ny in arround:
+            walk(nx, ny, minimum, step + 1)
+
+    for x, y in walls:
+        # find walls neighbours
+        arround = [(x + dx, y + dy) for dx, dy in [[-1, 0], [1, 0], [0, -1], [0, 1]]]
+        valid = [node for node in arround if node in visited]
+        #check distances at neighbours
+        localWeights = [weight for node, weight in visited.items() if node in valid]
+        if len(localWeights) != 0:
+            minimum = min(localWeights)
+            walk(x, y, minimum, 0)
+
+
+    print([qty for score, qty in scores.items() if score >= 50])
+    return sum([qty for score, qty in scores.items() if score >= 50])
+
+#a = calc1("day20.txt")
+#print("part 1:", a)
+
+a = calc2("day20.test")
+print("part 2:", a)
--- 2024/day21.py
+++ 2024/day21.py
@@ -5,7 +5,45 @@ class BigKeypad:
         self.cache = dict()
         self.x = 2
         self.y = 3
-        self.k = {"7": (0,0), "8": (1,0), "9": (2, 0), "4":(0,1), "5":(1,1), "6":(2,1), "1":(0,2), "2":(1,2), "3":(2,2), "0":(1,3), "A":(2,3)}
+        self.act = "A"
+        self.k = {"7": (0,0), "8": (1,0), "9": (2, 0), "4":(0,1), "5":(1,1), "6":(2,1), "1":(0,2), "2":(1,2), "3":(2,2), "0":(1,3), "A":(2,3), "F":(0,3)}
+
+        self.paths = set()
+        ps, pe = for ps in self.k for pe in self.k:
+            if ps == pe:
+                self.paths.add((ps, pe, "A"))
+                continue
+            dx = self.k[ps][0] - self.k[pe][0]
+            dy = self.k[ps][1] - self.k[pe][1]
+            if dx > 0:
+                progx = "<"
+            else
+                progx = ">"
+            if dy > 0:
+                progy = "^"
+            else:
+                progy = "v"
+            self.paths.add((ps, pe, progx * abs(dx) + progy * abs(dy) + "A"))
+            self.paths.add((ps, pe, progy * abs(dy) + progx * abs(dx) + "A"))
+
+        # remove paths which lead through forbidden F button
+        for ps, pe, path in self.paths:
+            x, y = self.k[ps]
+            for i in path:
+                if i == ">": x += 1
+                if i == "<": x -= 1
+                if i == "v": y += 1
+                if i == "^": y -= 1
+                if (x, y) = self.k["F"]:
+                    self.paths.remove((ps, pe, path))
+                    break
+
+
+    def press(self, c):
+        for ps, pe, path in self.paths:
+            if ps == self.act and pe == c:
+
+
 
 
     def code(self, code):
@@ -15,7 +53,7 @@ class BigKeypad:
             totalSignal += "A"
         return totalSignal
 
-    def press(self, c, prev = "<"):
+    def press1(self, c):
         x, y = self.k[c]
         origX = self.x
         origY = self.y
